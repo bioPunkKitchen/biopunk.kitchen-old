2736ce99d18365bf77cdaf6feb607556d04df274
speexy <jennifer.meyer@posteo.de>
2019-02-13 18:30:50 +0100

    some eslinting

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 import NavMain from './NavMain';
 import Hamburger from './Hamburger';
 import styles from './header.module.css';
 
 
-export const Header = ( props ) => (
+export const Header = () => (
     <header className={styles.header}>
-      <Link to={'/'}> SiteName </Link>
-      <NavMain/>
-      <Hamburger/>
+        <Link to={'/'}> SiteName </Link>
+        <NavMain/>
+        <Hamburger/>
     </header>
 );
--- ui/src/components/NavMain.js
+++ ui/src/components/NavMain.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Link } from 'gatsby';
 import styles from './navMain.module.css';
 
-const NavMain = (props) => {
+const NavMain = ( props ) => {
 
-  const toggle = props.closeOverlay || undefined;
-  const style = props.style === undefined ?  styles.navMain : props.style;
-  console.log(props)
+    const toggle = props.closeOverlay || undefined;
+    const style = props.style === undefined ? styles.navMain : props.style;
+    console.log( props );
 
-  return (
-    <nav className={style}>
-      <ul>
-          <Link to={'/blog'} className={styles.link} activeClassName={styles.active} onClick={toggle}>
+    return (
+        <nav className={style}>
+            <ul>
+                <Link to={'/blog'} className={styles.link} activeClassName={styles.active} onClick={toggle}>
               Blog
-          </Link>
-      </ul>
-    </nav>
-  )
+                </Link>
+            </ul>
+        </nav>
+    );
 };
 export default NavMain;
