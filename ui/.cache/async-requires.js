68b97d4bf7be95cd9f2fc2d0af0be60b90172811
speexy <jennifer.meyer@posteo.de>
2019-02-13 20:27:40 +0100

    really need to ignore cache...

--- ui/.cache/async-requires.js
+++ ui/.cache/async-requires.js
@@ -2,8 +2,9 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---src-templates-blog-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/src/templates/blog.js" /* webpackChunkName: "component---src-templates-blog-js" */),
   "component---src-pages-index-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-templates-blog-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/src/templates/blog.js" /* webpackChunkName: "component---src-templates-blog-js" */),
+  "component---src-templates-post-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/src/templates/post.js" /* webpackChunkName: "component---src-templates-post-js" */),
   "component---cache-dev-404-page-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/.cache/dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-legal-js": () => import("/home/jenn/coding/jenns-gatsby-starter/ui/src/pages/legal.js" /* webpackChunkName: "component---src-pages-legal-js" */)
--- ui/.cache/sync-requires.js
+++ ui/.cache/sync-requires.js
@@ -5,8 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---src-templates-blog-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/src/templates/blog.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/src/pages/index.js"))),
+  "component---src-templates-blog-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/src/templates/blog.js"))),
+  "component---src-templates-post-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/src/templates/post.js"))),
   "component---cache-dev-404-page-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/src/pages/404.js"))),
   "component---src-pages-legal-js": hot(preferDefault(require("/home/jenn/coding/jenns-gatsby-starter/ui/src/pages/legal.js")))
