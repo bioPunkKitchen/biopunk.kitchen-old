8e2c0a3bea8fea4d0eca263d11ea9837963b7331
speexy <jennifer.meyer@posteo.de>
2019-02-14 15:51:03 +0100

    update dependencies, remove gatsby image stuff

--- ui/gatsby-config.js
+++ ui/gatsby-config.js
@@ -31,8 +31,6 @@ module.exports = {
                 }
             }
         },
-        'gatsby-plugin-sharp',
-        'gatsby-transformer-sharp',
         {
             resolve: 'gatsby-transformer-remark',
             options: {
