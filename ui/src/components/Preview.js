729567c166ecfbb0fe10723aa24e9e1b43ab3b5f
speexy <jennifer.meyer@posteo.de>
2019-03-10 23:23:27 +0100

    add preview component

---
+++ ui/src/components/Preview.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Hero from './Hero';
+import styles from './preview.module.css';
+
+const Preview = ( props ) => (
+    <div className={styles.preview}>
+        {
+
+            props.projects.map(( project, index ) =>
+                <article key={index}>
+                    <Hero image={project.node.frontmatter.image} title={project.node.frontmatter.title} />
+                    <div style={{ backgroundImage: `url(${project.node.frontmatter.image})` }}></div>
+                    <div></div>
+                    <p>{project.node.frontmatter.summary}</p>
+                    <Link to={project.node.fields.slug} > ... more </Link>
+                </article> )}
+
+    </div>
+);
+
+export default Preview;
