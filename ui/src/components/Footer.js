95cce5ecf59fa9edc6c76f0e38a86f50418695b3
speexy <jennifer.meyer@posteo.de>
2019-03-10 23:52:28 +0100

    add map to footer

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Map from '../../static/media/map.png';
 import styles from './footer.module.css';
 
 const Footer = () => (
     <div className={styles.footer}>
-        <Link to={'/legal'} > Imprint & Privacy </Link>
+        <a href="https://www.openstreetmap.de/karte.html?zoom=15&lat=52.38885&lon=13.07688&layers=B000" target="_black" style={{ backgroundImage: `url(${Map})` }}></a>
+        <div>
+            <Link to={'/legal'} > Imprint & Privacy </Link>
+        </div>
     </div>
 );
 
