12277b882f7e3786ebbe26b65d7f548ff9092d64
speexy <jennifer.meyer@posteo.de>
2019-03-11 02:27:41 +0100

    since button so far only for newsletter, always open in new tab

--- ui/src/components/Button.js
+++ ui/src/components/Button.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styles from './button.module.css';
 
 const Button = ( props ) => (
-    <a href={props.url} className={styles.button}> {props.label} </a>
+    <a href={props.url} target="_blank" className={styles.button}> {props.label} </a>
 );
 
 export default Button;
